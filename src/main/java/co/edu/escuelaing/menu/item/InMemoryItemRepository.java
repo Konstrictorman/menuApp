ee1323b2a782ce9b8f37c3bcae9ad9a47b5d124e
Ricardo Álvarez <edralvar@bancolombia.com>
2023-11-22 11:18:26 -0500

    Initial commit

---
+++ src/main/java/co/edu/escuelaing/menu/item/InMemoryItemRepository.java
@@ -0,0 +1,7 @@
+package co.edu.escuelaing.menu.item;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InMemoryItemRepository extends CrudRepository<Item, Long> {}
